31636a47b2ad414753aaf8283d6f71a89bbfe7e1
Papajohn77 <t8190314@aueb.gr>
2023-12-17 00:03:09 +0200

    Fix OpenAPI by providing the server URL.

--- vrp-solver-api/src/main/java/tech/johnpapadatos/vrpsolverapi/config/OpenAPIConfig.java
+++ vrp-solver-api/src/main/java/tech/johnpapadatos/vrpsolverapi/config/OpenAPIConfig.java
@@ -1,20 +1,30 @@
 package tech.johnpapadatos.vrpsolverapi.config;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import io.swagger.v3.oas.models.servers.Server;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 
 @Configuration
 public class OpenAPIConfig {
-    
+
     @Bean
-    public OpenAPI openAPI() {
+    public OpenAPI openAPI(
+        @Value("${server.url}") String serverUrl
+    ) {
+        Server server = new Server();
+        server.setUrl(serverUrl);
+        server.setDescription("Server URL");
+
         Info info = new Info()
             .title("VRP Solver API")
             .version("0.1.0");
 
-        return new OpenAPI().info(info);
+        return new OpenAPI().info(info).servers(List.of(server));
     }
 }
